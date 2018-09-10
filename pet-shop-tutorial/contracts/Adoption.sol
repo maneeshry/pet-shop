67a91d3e88c9dd68f5bb4411f1d36f179daec90c
7330967478 <yalamareddymaneesh@gmail.com>
2018-09-10 19:55:21 +0530

    commit

---
+++ pet-shop-tutorial/contracts/Adoption.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.17;
+
+contract Adoption {
+    address[16] public adopters;
+    function adopt(uint petId) public returns (uint){
+        require(petId>=0 && petId<=15);
+        adopters[petId]=msg.sender;
+        return petId;
+    }
+    function getadopters() public view returns(address[16]) {
+        return adopters;   
+    }
+}
